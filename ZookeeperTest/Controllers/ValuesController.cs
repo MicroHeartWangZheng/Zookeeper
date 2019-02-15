587cc45951590ba44709f9697a8836d2e449e64f
王郑 <418706636@qq.com>
2019-02-15 17:23:10 +0800

    add

--- ZookeeperTest/Controllers/ValuesController.cs
+++ ZookeeperTest/Controllers/ValuesController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using org.apache.zookeeper;
+using static org.apache.zookeeper.ZooDefs;
 
 namespace ZookeeperTest.Controllers
 {
@@ -10,36 +13,31 @@ namespace ZookeeperTest.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
+            MyWatcher myWatcher = new MyWatcher();
+            ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 100, myWatcher);
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+            var stat = zk.existsAsync("/root", true);
+            //创建一个节点root，数据是mydata,不进行ACL权限控制，节点为永久性的(即客户端shutdown了也不会消失) 
+            //zk.Create("/root", "mydata".GetBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.Persistent);
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            //在root下面创建一个childone znode,数据为childone,不进行ACL权限控制，节点为永久性的 
+            zk.createAsync("/root/childone", Encoding.UTF8.GetBytes("childone"), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            //取得/root节点下的子节点名称,返回List<String> 
+            zk.getChildrenAsync("/root", true);
+            //取得/root/childone节点下的数据,返回byte[] 
+            zk.getDataAsync("/root/childone", myWatcher);
+
+            //修改节点/root/childone下的数据，第三个参数为版本，如果是-1，那会无视被修改的数据版本，直接改掉
+            zk.setDataAsync("/root/childone", Encoding.UTF8.GetBytes("childonemodify"), -1);
+            //删除/root/childone这个节点，第二个参数为版本，－1的话直接删除，无视版本 
+            zk.deleteAsync("/root/childone", -1);
+
+            return null;
         }
     }
 }
---
+++ ZookeeperTest/MyWatcher.cs
@@ -0,0 +1,19 @@
+﻿using org.apache.zookeeper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZookeeperTest
+{
+    public class MyWatcher : Watcher
+    {
+        public override Task process(WatchedEvent @event)
+        {
+            Console.WriteLine($@"Path:{@event.getPath()}");
+            Console.WriteLine($@"State:{@event.getState()}");
+            Console.WriteLine($@"Type:{@event.GetType()}");
+            return null; 
+        }
+    }
+}
