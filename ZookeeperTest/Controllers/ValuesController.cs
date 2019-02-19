a661a65c98bef2d854956c168cd1cf49985a38fd
王郑 <418706636@qq.com>
2019-02-19 18:15:32 +0800

    fix

--- ZookeeperTest/Controllers/ValuesController.cs
+++ ZookeeperTest/Controllers/ValuesController.cs
@@ -19,9 +19,7 @@ namespace ZookeeperTest.Controllers
 
             MyWatcher myWatcher = new MyWatcher();
 
-            MyWatcher myWatcher1 = new MyWatcher();
-            MyWatcher myWatcher2 = new MyWatcher();
-            ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 100, myWatcher);
+            ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 10000, myWatcher);
 
 
             var stat = zk.existsAsync("/root", true);
@@ -31,15 +29,16 @@ namespace ZookeeperTest.Controllers
             //在root下面创建一个childone znode,数据为childone,不进行ACL权限控制，节点为永久性的 
             zk.createAsync("/root/childone", Encoding.UTF8.GetBytes("childone"), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             //取得/root节点下的子节点名称,返回List<String> 
-            zk.getChildrenAsync("/root", myWatcher1);
+            zk.getChildrenAsync("/root", true);
             //取得/root/childone节点下的数据,返回byte[] 
-            zk.getDataAsync("/root/childone", myWatcher2);
+            zk.getDataAsync("/root/childone", true);
 
             //修改节点/root/childone下的数据，第三个参数为版本，如果是-1，那会无视被修改的数据版本，直接改掉
             zk.setDataAsync("/root/childone", Encoding.UTF8.GetBytes("childonemodify"), -1);
             //删除/root/childone这个节点，第二个参数为版本，－1的话直接删除，无视版本 
             zk.deleteAsync("/root/childone", -1);
 
+            zk.existsAsync("/root", true);
             return null;
         }
     }
